8e6773aea426e4b156f279ab5defa8fe2dca7dae
chenshoumao <1223991507@qq.com>
2018-08-14 11:42:18 +0800

    23

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-#编译+部署m2站点
+#编译+部署m2站点 
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 ### base 函数
-killTomcat(){
+killTomcat()
+{
     pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
     echo "tomcat Id list :$pid"
     if [ "$pid" = "" ]
@@ -18,11 +19,14 @@ killTomcat(){
 }
 cd $PROJ_PATH/m2
 mvn clean install
+
 # 停tomcat
 killTomcat
+
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/m2
 rm -f $TOMCAT_APP_PATH/webapps/m2.war
+
 # 复制新的工程
 cp $PROJ_PATH/m2/target/m2.war $TOMCAT_APP_PATH/webapps/
 # 启动Tomcat
